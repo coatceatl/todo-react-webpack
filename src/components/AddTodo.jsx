342b18920bab84f829a764e78659a2bc64eadbd5
Oksana Tkachenko <oksana@crystax.net>
2019-02-26 17:02:42 +0200

    added component AddTodo

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AddTodo = () => {
+class AddTodo extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      content: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      content: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
     return (
-      <div>
-        AddTodo
+      <div className="todo-content">
+        <form onSubmit={this.handleSubmit}>
+          <label>Add new todo:</label>
+          <input type="text"
+            onChange = {this.handleChange}
+            value = {this.state.value}
+          />
+        </form>
       </div>
     );
+  }
 }
 
 export default AddTodo
